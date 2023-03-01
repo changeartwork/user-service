c145d8152bec45901768ea55fd44e95ad9a50c83
neerap-india <tony.dev@changeartwork.com>
2023-03-02 00:27:37 +0530

    CMA-13 : Client registration model added

---
+++ model/client.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+
+const clientSchema = new mongoose.Schema(
+  {
+    client_id: {
+      type: String,
+      unique: [true, "Client ID already exists in database"],
+      required: [true, "Client ID is manditory"]
+    },
+    profile: [{
+      first_name: {
+        type: String,
+        required: [true, "First Name is required"],
+      },
+      last_name: {
+        type: String,
+        required: [true, "Last Name is required"],
+      },
+      email: {
+        type: String,
+        unique: [true, "Email already exists in database"],
+        lowercase: true,
+        trim: true,
+        required: [true, "Email not provided"],
+        validate: {
+          validator: function (v) {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+          },
+          message: '{VALUE} is not a valid email!'
+        }
+      },
+      avatar: {
+        type: String
+      },
+      profile_status: {
+        type: String,
+        default: "INACTIVE",
+        enum: ["ACTIVE", "INACTIVE"]
+      }
+    }],
+    role: {
+      type: String,
+      default: "client"
+    },
+    client_status: {
+      type: String,
+      default: "INACTIVE",
+      enum: ["ACTIVE", "INACTIVE"]
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    business_name: {
+      type: String
+    },
+    phone_number: {
+      type: String,
+      required: [true, "Phone number is required"]
+    },
+    payment_terms_in_days: {
+      type: String,
+      enum: ["7", "15", "30"],
+      required: [true, "Payment terms is required"]
+    },
+    currency_code: {
+      type: String,
+      enum: ["USD", "CAD", "GBP"],
+      required: [true, "Currency code is required"]
+    },
+    payment_mode: {
+      type: String,
+      enum: ["CC", "CCF", "PP", "CQ", "ACT"],
+      required: [true, "Payment mode is required"]
+    },
+    city: {
+      type: String,
+      required: [true, "City is required"]
+    },
+    state: {
+      type: String,
+      required: [true, "State is required"]
+    },
+    country: {
+      type: String,
+      required: [true, "Country code is required"]
+    },
+    zipcode: {
+      type: String,
+      required: [true, "Zip Code is required"]
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('client', clientSchema);
