709a4142e86eefc27e7481080d7fc6888051e23f
neerap-india <tony.dev@changeartwork.com>
2023-03-15 01:11:20 +0530

    removed deprecated endpoints

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const userRoute = require('./routes/endpoints');
 const profile = require('./routes/profile');
 const registerClient = require('./routes/register-client');
 const loginClient = require('./routes/login-client');
@@ -15,10 +14,11 @@ require('./config/db');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/v1/api/user',userRoute, profile, registerClient, loginClient, forgotPassword);
+app.use('/v1/api/user', profile, registerClient, loginClient, forgotPassword);
 app.use(cors());
 app.use('/v1/api/user/spec', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCss}));
 
 app.listen(`${process.env.APP_PORT}`, () => {
   console.log('user-service started on port '+ `${process.env.APP_PORT}`);
 });
+ 
